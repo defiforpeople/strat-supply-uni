9588d8c34e79dc4399098b3f36593a70cf1ea3d1
NicoBarragan <nbarragan01@gmail.com>
2022-08-20 19:05:52 -0300

    feat(retrieveNFT.ts): created script for user to retrieve NFT of the position from the SupplyUni contract

---
+++ scripts/retrieveNFT.ts
@@ -0,0 +1,27 @@
+import { ethers } from "hardhat";
+import { BigNumber, ContractTransaction } from "ethers";
+import { SupplyUni } from "../typechain-types";
+const logger = require("pino")();
+
+const { CONTRACT_ADDRESS } = process.env;
+const GAS_LIMIT = BigNumber.from("2074000");
+
+export default async function retrieveNFT(
+  poolId: BigNumber,
+  userAddr: string
+): Promise<ContractTransaction> {
+  const gas = { gasLimit: GAS_LIMIT };
+
+  const user = await ethers.getSigner(userAddr);
+  const supplyUni = (await ethers.getContractAt(
+    "SupplyUni",
+    CONTRACT_ADDRESS!
+  )) as SupplyUni;
+
+  logger.info("Retrieving NFT of the position...");
+  const tx = await supplyUni.connect(user).retrieveNFT(poolId, gas);
+  await tx.wait();
+  logger.info("Position retrieved!");
+
+  return tx;
+}
