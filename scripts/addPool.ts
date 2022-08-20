6c75969dc46828a6ce7065548554cca6f79a9acd
NicoBarragan <nbarragan01@gmail.com>
2022-08-20 19:06:50 -0300

    refactor(addPool.ts): imported contract address from env values and added gas object var

--- scripts/addPool.ts
+++ scripts/addPool.ts
@@ -3,18 +3,19 @@ import { BigNumber } from "ethers";
 import { SupplyUni } from "../typechain-types";
 const logger = require("pino")();
 
+const { CONTRACT_ADDRESS } = process.env;
 const GAS_LIMIT = BigNumber.from("2074000");
-const gas = { gasLimit: GAS_LIMIT };
+
 const addPool = async (
-  conractAddr: string,
   token0Addr: string,
   token1Addr: string,
   poolFee: BigNumber
 ) => {
+  const gas = { gasLimit: GAS_LIMIT };
   const [owner] = await ethers.getSigners();
   const supplyUni = (await ethers.getContractAt(
     "SupplyUni",
-    conractAddr
+    CONTRACT_ADDRESS!
   )) as SupplyUni;
 
   logger.info("Adding new pool...");
