fc3a63508c0ea567de2cae1bfc3b9d9bdff6858e
NicoBarragan <nbarragan01@gmail.com>
2022-08-20 18:56:59 -0300

    hotfix(decreaseLoop.ts): fixed script

--- utils/decreaseLoop.ts
+++ utils/decreaseLoop.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 const logger = require("pino")();
 
 const { CONTRACT_ADDRESS } = process.env;
-const GAS_LIMIT = BigNumber.from("2074040");
+const GAS_LIMIT = BigNumber.from("2074000");
 let tx: ContractTransaction;
 
 export default async function decreaseLoop(
@@ -11,30 +11,25 @@ export default async function decreaseLoop(
   userAddr: string,
   liquidityRemaining: BigNumber,
   liquidityNeeded: BigNumber,
-  maxSlip: BigNumber,
-  contractAddr?: string // for unit tests in local
+  maxSlip: BigNumber
 ): Promise<ContractTransaction> {
-  const strategyAddr = CONTRACT_ADDRESS ? CONTRACT_ADDRESS : contractAddr;
-  const supplyUni = await ethers.getContractAt("SupplyUni", `${strategyAddr}`);
-  logger.info(`strat addr: ${supplyUni.address}`);
-
+  const supplyUni = await ethers.getContractAt("SupplyUni", CONTRACT_ADDRESS!);
   const user = await ethers.getSigner(userAddr);
-  logger.info(`userAddr: ${user.address}`);
-
-  logger.info(`liquidity remaining: ${liquidityRemaining}`);
-  logger.info(`liquidity needed   : ${liquidityNeeded}`);
 
   let liquidity = liquidityRemaining;
-  let neededLiq = liquidityNeeded.sub(liquidity);
+  let neededLiq = liquidityRemaining.sub(liquidityNeeded);
+  const expectedLiq = liquidityNeeded;
   let count = 0;
-  while (liquidity.lt(liquidityNeeded) && liquidity.gt(GAS_LIMIT)) {
+  while (liquidity.gt(expectedLiq) && liquidity.gt(GAS_LIMIT)) {
     // for accounting the tx needed
     count += 1;
     logger.info(`count: ${count}`);
 
     // get the percentage to decrease by the dif between the liquidity
     // in Uniswap and the liquidity that is needed
-    let percentageNeeded = neededLiq.mul(BigNumber.from(100)).div(neededLiq);
+    let percentageNeeded = neededLiq
+      .mul(BigNumber.from(100))
+      .div(liquidityRemaining);
 
     percentageNeeded = percentageNeeded.add(1).lt(100)
       ? percentageNeeded.add(1)
@@ -56,13 +51,12 @@ export default async function decreaseLoop(
       userAddr,
       poolId
     );
-    logger.info(`liqUserAfter: ${liqUserAfter}`);
 
     liquidity = liqUserAfter;
     neededLiq = neededLiq.sub(liqBefore.sub(liqUserAfter));
   }
 
-  logger.info(`liquidity: ${liquidity}`);
-  logger.info(`neededLiq: ${neededLiq}`);
+  logger.info(`liquidity at the end of the loop: ${liquidity}`);
+  logger.info(`neededLiq at the end of the loop: ${neededLiq}`);
   return tx;
 }
